3aa5154fd57643330c867d14f5bb4190cd3abe7b
1785301169@qq.com <1785301169@qq.com>
2018-12-06 16:04:27 +0800

    新增收货地址

--- routes/web.php
+++ routes/web.php
@@ -21,12 +21,14 @@ Route::group(['middleware' => 'auth'], function() {
     Route::get('/email_verification/verify', 'EmailVerificationController@verify')->name('email_verification.verify');
     Route::get('/email_verification/send', 'EmailVerificationController@send')->name('email_verification.send');
 
-    // 开始
+    // start
     Route::group(['middleware' => 'email_verified'], function() {
 
         Route::get('user_addresses', 'UserAddressesController@index')->name('user_addresses.index');
+        Route::get('user_addresses/create', 'UserAddressesController@create')->name('user_addresses.create');
+        Route::post('user_addresses', 'UserAddressesController@store')->name('user_addresses.store');
 
     });
-    // 结束
+    // end
 
 });
