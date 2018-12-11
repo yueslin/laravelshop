32e4446237cadd692e1c6909596d6de972c53bbb
1785301169@qq.com <1785301169@qq.com>
2018-12-11 15:05:24 +0800

    创建订单

--- routes/web.php
+++ routes/web.php
@@ -32,13 +32,13 @@ Route::group(['middleware' => 'auth'], function() {
         Route::get('user_addresses/{user_address}', 'UserAddressesController@edit')->name('user_addresses.edit');
         Route::put('user_addresses/{user_address}', 'UserAddressesController@update')->name('user_addresses.update');
         Route::delete('user_addresses/{user_address}', 'UserAddressesController@destroy')->name('user_addresses.destroy');
-
         Route::post('products/{product}/favorite', 'ProductsController@favor')->name('products.favor');
         Route::delete('products/{product}/favorite', 'ProductsController@disfavor')->name('products.disfavor');
         Route::get('products/favorites', 'ProductsController@favorites')->name('products.favorites');
         Route::post('cart', 'CartController@add')->name('cart.add');
         Route::get('cart', 'CartController@index')->name('cart.index');
         Route::delete('cart/{sku}', 'CartController@remove')->name('cart.remove');
+        Route::post('orders', 'OrdersController@store')->name('orders.store');
 
     });
     // end
