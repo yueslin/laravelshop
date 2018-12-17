45b4e32e73e9524a01364cb554b49e8baf20ee4d
1785301169@qq.com <1785301169@qq.com>
2018-12-17 11:50:41 +0800

    管理后台 - 订单列表

--- resources/views/products/index.blade.php
+++ resources/views/products/index.blade.php
@@ -37,6 +37,10 @@
                     <div class="title">
                       <a href="{{ route('products.show', ['product' => $product->id]) }}">{{ $product->title }}</a>
                     </div>
+                    <div class="bottom">
+                      <div class="sold_count">销量 <span>{{ $product->sold_count }}笔</span></div>
+                      <div class="review_count">评价 <span>{{ $product->review_count }}</span></div>
+                    </div>
                   </div>
                 </div>
               </div>
