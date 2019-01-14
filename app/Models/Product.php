7d76031198a6d3d307293c8f7599b61e057f24be
1785301169@qq.com <1785301169@qq.com>
2019-01-14 17:06:07 +0800

    商品添加长标题字段

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -17,7 +17,7 @@ class Product extends Model
     ];
     
     protected $fillable = [
-        'title', 'description', 'image', 'on_sale',
+        'title', 'description', 'image', 'on_sale','long_title',
         'rating', 'sold_count', 'review_count', 'price','type'
     ];
 
